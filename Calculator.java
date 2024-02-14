cff668cb67ef86254efe467524e0ba9e65bc52ab
Zbroadbent2 <zachbroadbent04@gmail.com>
2024-02-13 22:16:10 -0500

    Added addition

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first whole number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second whole number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+
+        scanner.close();
+    }
+}

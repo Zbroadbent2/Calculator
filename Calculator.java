10992099f14a5dcace8345a7691273ffcff5aa70
Zbroadbent2 <zachbroadbent04@gmail.com>
2024-04-11 15:54:55 -0400

    Addition of two whole numbers

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first whole number: ");
+    int num1 = scanner.nextInt();
+    System.out.println("Enter the second whole numnber: ");
+    int num2 = scanner.nextInt();
+
+    int sum = num1 + num2;
+    System.out.println("The sum is: " + sum);
+
+    scanner.close();
     }
 }
